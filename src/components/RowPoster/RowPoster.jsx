ff0ea09a833fda2cf9c983f352ee877ecc84c231
hamzakhankakar <khan4web1@gmail.com>
2025-08-03 03:50:47 +0500

    ui changed

--- src/components/RowPoster/RowPoster.jsx
+++ src/components/RowPoster/RowPoster.jsx
@@ -1,182 +1,10 @@
-// // import "./rowPoster.scss";
-// // import { useDispatch } from "react-redux";
-// // import { showModalDetail } from "../../redux/modal/modal.actions";
-// // import { Link } from "react-router-dom";
-// //  import {  FaPlay, FaChevronDown } from "react-icons/fa";
-// //  import { MdReadMore } from "react-icons/md";
-
-
-// // const RowPoster = result => {
-// // 	const {  item: { title, original_name, original_title, name,  
-// //   }
-// //    } = result;
-// // 	let fallbackTitle = title || original_title || name || original_name;
-// // 	const dispatch = useDispatch();
-// // 	const handleModalOpening = () => {
-// // 		dispatch(showModalDetail({ ...result.item, fallbackTitle,}));
-// // 	}
-// // 	const handlePlayAction = event => {
-// // 		event.stopPropagation();
-
-// // 	};
-
-// // 	return (
-
-// //     <div  className="Row__poster"  >
-// //         <div className="item">
-// //     <img  src={`${result.item?.images[0]}`}
-// //         alt={fallbackTitle} />
-// //     <div>
-      
-// //       {result.item?.trending}
-// //     </div>
-// //  </div>
-     
-
-       
-// // 		<div className="Row__poster-info" >
-// // 		{result?.item.type === "property" && (
-// //   <div className="Row__poster-info--iconswrp">
-// //     <Link
-// //       className="Row__poster-info--icon icon--play"
-// //       onClick={handlePlayAction}
-// //       to={`/play`}
-// //     >
-// //       <FaPlay />
-// //     </Link>
-
-// //     <button className="Row__poster-info--icon icon--toggleModal" onClick={handleModalOpening}>
-// //       <FaChevronDown  />
-// //     </button>
-
-// //     <Link to={`/property/${result.item?.id}`} style={{ display: 'block' }}>
-// //       {/* <button
-// //                     className="Banner__button "
-// //                   > */}
-
-
-                    
-// //                     <span className="Row__poster-info--icon icon--toggleModal"> 
-                      
-// //                     <MdReadMore size={"1em"} />
-// //                     </span>
-                  
-// //                     </Link>
-// //   </div>
-// // )}
-
-// // 		</div>
-    
-// //   </div>
-	
-// // 	);
-// // };
-
-// // export default RowPoster;
-	
-
-// import "./rowPoster.scss";
-// // import { useState } from "react";
-// import { useDispatch } from "react-redux";
-// import { showModalDetail } from "../../redux/modal/modal.actions";
-// import { Link } from "react-router-dom";
-// import { FaPlay, FaChevronDown } from "react-icons/fa";
-// import { MdReadMore } from "react-icons/md";
-
-// const RowPoster = (result) => {
- 
-  
-//   const {
-//     item: { title, original_name, original_title, name },
-//   } = result;
-//   let fallbackTitle = title || original_title || name || original_name;
-//   const dispatch = useDispatch();
-
-//   const handleModalOpening = () => {
-//     dispatch(showModalDetail({ ...result.item, fallbackTitle }));
-//   };
-
-//   const handlePlayAction = (event) => {
-//     event.stopPropagation();
-//   };
-  
-//   return (
-  
-//     <div className="Row__poster"
-
-//     >
-
-//       {/* Property card */}
-//       <div className="image-wrapper">        
-//         <img src={`${result.item?.images[0]}`} alt={fallbackTitle} />
-//         <div>{result.item?.trending}</div>
-//       </div>
-
-//       <div className={`Row__poster-info 
-        
-//         `}>        
-//            {(result?.item.type === "property" || result?.item.type === "blog") && (
-//           <>
-//               <div className="Row__poster-info--iconswrp">
-//                  { result?.item.type === "property" && <Link
-//                   className="Row__poster-info--icon icon--play"
-//                   onClick={handlePlayAction}
-//                   to={`/play`}
-//                 >
-//                   <FaPlay />
-//                 </Link>}
-
-//               {  result?.item.type === "property" &&  <button
-//                   className="Row__poster-info--icon icon--toggleModal"
-//                   onClick={handleModalOpening}
-//                 >
-//                   <FaChevronDown />
-//                 </button>}
-
-//                 {result.item?.type === "property" ?
-//                 <Link
-//                 to={`/property/${result.item?.id}`}
-//                 style={{ display: "block" }}
-//               >
-            
-
-//                 <span className="Row__poster-info--icon icon--toggleModal">
-//                   <MdReadMore size={"1em"} />
-//                 </span>
-//               </Link>:
-//               <Link
-//               to={`/blog`}
-//               style={{ display: "block" }}
-//             >
-          
-
-//               <span className="Row__poster-info--icon icon--toggleModal">
-//                 <MdReadMore size={"1em"} />
-//               </span>
-//             </Link>}
-//               </div>
-//               <div className="Row__poster-info--title">
-//               <h3>{fallbackTitle}</h3>
-//             </div>
-//          </>
-//         )}
-//       </div>
-//     </div>
-
-//     // </div>
-//   );
-// };
-
-// export default RowPoster;
-
-
 import {useState} from "react"
 import "./rowPoster.scss";
 import { useDispatch } from "react-redux";
 import { showModalDetail } from "../../redux/modal/modal.actions";
-import { Link } from "react-router-dom";
-import {  FaPlay, FaChevronDown } from "react-icons/fa";
-import { MdReadMore } from "react-icons/md";
+// import { Link } from "react-router-dom";
+// import {  FaPlay, FaChevronDown } from "react-icons/fa";
+// import { MdReadMore } from "react-icons/md";
 
 
 
@@ -187,28 +15,27 @@ const RowPoster = (result) => {
   const fallbackTitle = title || original_title || name || original_name;
   const dispatch = useDispatch();
 
-  const [isInfoOpen, setIsInfoOpen] = useState(false);
+  // const [isInfoOpen, setIsInfoOpen] = useState(false);
   
-  const handleToggleInfo = () => {
-    setIsInfoOpen(prev => !prev);
-  };
+  // const handleToggleInfo = () => {
+  //   setIsInfoOpen(prev => !prev);
+  // };
 
-  const handleModalOpening = () => {
-    dispatch(showModalDetail({ ...result.item, fallbackTitle }));
-  };
+  // const handleModalOpening = () => {
+    // dispatch(showModalDetail({ ...result.item, fallbackTitle }));
+  // };
 
-  const handleIconClick = (e) => {
-    e.stopPropagation(); // prevent overlay toggle
-    // You can add more logic here if needed
-  };
+  // const handleIconClick = (e) => {
+  //   e.stopPropagation(); // prevent overlay toggle
+  //   // You can add more logic here if needed
+  // };
 
   return (
     <div
       className="Row__poster"
-      onClick={() => {
-        // Toggle overlay on image click (mobile)
-        handleToggleInfo();
-      }}
+      // onClick={() => {
+      //   handleToggleInfo();
+      // }}
     >
       {/* Image wrapper */}
       <div className="image-wrapper">
@@ -217,7 +44,7 @@ const RowPoster = (result) => {
       </div>
 
       {/* Info overlay */}
-      <div
+      {/* <div
         className="Row__poster-info"
         style={{
           opacity: isInfoOpen ? 1 : 0,
@@ -270,8 +97,29 @@ const RowPoster = (result) => {
             </div>
           </>
         )}
-      </div>
+      </div> */}
     </div>
   );
 };
 export default RowPoster;
+
+
+
+
+// 3:4 (e.g., 300x400 pixels)
+// 4:5 (e.g., 400x500 pixels)
+// 2:3 (e.g., 200x300 pixels)
+// 9:16 (used for mobile screens and stories)
+
+// .portrait-container {
+//   width: 300px; /* or any width you prefer */
+//   aspect-ratio: 3 / 4; /* 3:4 aspect ratio */
+//   overflow: hidden;
+//   border-radius: 8px;
+// }
+
+// .portrait-container img {
+//   width: 100%;
+//   height: 100%;
+//   object-fit: cover; /* ensures the image covers the container */
+// }
